dacbe632a7a3b34df211ece0570513633987f4c1
samirm00 <sammoham.be@gmail.com>
2023-08-10 05:12:52 +0200

    initial commit

--- src/handlers/sumHandler.js
+++ src/handlers/sumHandler.js
@@ -1,3 +1,16 @@
+/**
+ * Handles the calculation of the sum of two numbers obtained from the DOM.
+ * The function retrieves the numbers from input fields defined in the `dom` module,
+ * calculates their sum using the `sum` utility, and then displays the result
+ * in the specified container. If the result container doesn't exist, it creates one
+ * using `createSum`; if it does, it updates the existing container with the new result
+ * using `updateSum`.
+ *
+ * @function
+ * @example
+ * sumHandler();
+ */
+
 import dom from '../dom.js';
 import sum from '../utils/sum.js';
 import createSum from '../components/createSum.js';
